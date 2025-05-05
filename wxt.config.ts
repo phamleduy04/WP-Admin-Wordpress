3fecea10a43ce64722ce72c6db2d793cd44467fa
Duy Pham Le <32657584+phamleduy04@users.noreply.github.com>
2025-05-05 15:54:50 -0500

    change ext name and add icons

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,11 +2,16 @@ import { defineConfig } from 'wxt';
 
 export default defineConfig({
   manifest: {
-    name: 'WordPress Admin Switcher',
-    description: 'Switch between WordPress pages and their admin versions',
-    version: '1.0',
+    name: 'UT Dallas WordPress Admin Switcher',
+    description: 'Switch between WordPress pages and their admin versions (for UT Dallas websites)',
+    version: '1.1',
     permissions: ['tabs', 'activeTab', 'scripting'],
     host_permissions: ['*://*.utdallas.edu/*'],
+    icons: {
+      '16': 'images/icon16.png',
+      '48': 'images/icon48.png',
+      '128': 'images/icon128.png'
+    },
   },
   srcDir: '.',
   outDir: '.output',
