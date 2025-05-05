b6b9a1af2cd235b3bd1db57f7bac1fc01e762633
Duy Pham Le <32657584+phamleduy04@users.noreply.github.com>
2025-05-05 15:57:44 -0500

    fix icons

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,17 +4,18 @@ export default defineConfig({
   manifest: {
     name: 'UT Dallas WordPress Admin Switcher',
     description: 'Switch between WordPress pages and their admin versions (for UT Dallas websites)',
-    version: '1.1',
+    version: '1.1.1',
     permissions: ['tabs', 'activeTab', 'scripting'],
     host_permissions: ['*://*.utdallas.edu/*'],
     icons: {
-      '16': 'images/icon16.png',
-      '48': 'images/icon48.png',
-      '128': 'images/icon128.png'
+      '16': 'icon16.png',
+      '48': 'icon48.png',
+      '128': 'icon128.png'
     },
   },
   srcDir: '.',
   outDir: '.output',
+  publicDir: 'images',
   webExt: {
     startUrls: ['https://engineering.utdallas.edu/']
   }
